1124794b43f71dd3b57174eb18a7ae71ee1dc6df
Travis Rollins <TROLL59910@aol.com>
2017-08-10 17:41:30 -0600

    Add configuration for Sentry and Raven to display message when something goes wrong

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -15,6 +15,19 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import store, { history } from './store'
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'asdfas9d08f',
+    userLevel: 'editor'
+  }
+}).install();
+
+Raven.captureMessage('Something bad happened!');
+Raven.showReportDialog();
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
